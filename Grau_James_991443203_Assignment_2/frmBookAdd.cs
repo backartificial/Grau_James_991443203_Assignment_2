beff412aeb73492faf34750360b6b4e1b3e395fd
James Grau <backartificial@users.noreply.github.com>
2018-06-14 23:36:20 -0400

    Book Add Form Designed
    Designed for adding a book and have dataview pulling from DB

---
+++ Grau_James_991443203_Assignment_2/frmBookAdd.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Grau_James_991443203_Assignment_2 {
+    public partial class frmBookAdd : Form {
+        public frmBookAdd() {
+            InitializeComponent();
+        }
+    }
+}
